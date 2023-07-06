4f05793d47a6dfefb590268024bcd97b435522e5
arc6828 <chavalit.kow@gmail.com>
2023-07-06 17:03:57 +0700

    Initial commit

---
+++ gen.py
@@ -0,0 +1,27 @@
+import json 
+from faker import Faker
+import random
+from random import randint
+fake = Faker('en_US')
+a = []
+mode_option = {
+    "20KB" : 82,
+    "200KB" : 826,
+    "2000KB" : 8310,
+}
+# key = "20KB"
+# key = "200KB"
+key = "2000KB"
+
+for _ in range(mode_option[key]):
+    # my_dict = {    'foo': randint(0, 100),    'bar': {'baz': fake.name(),       'poo': float(random.randrange(155, 389))/100   } }
+    # print(my_dict)
+    my_dict = {
+        "name" : fake.name(),
+        "address" : fake.address(),
+        "text" : fake.text(),
+    }
+    a.append(my_dict)
+
+with open("dataset-"+key+".json", "w") as f:
+    json.dump(a, f)
---
+++ readme.md
@@ -0,0 +1,6 @@
+https://medium.com/ckartisan/python-101-b0e57af2f00a
+
+```python
+pip install -r requirements.txt
+```
+
