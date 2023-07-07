371571028727b3935086ed88b3c16cffecf04730
arc6828 <chavalit.kow@gmail.com>
2023-07-07 16:24:15 +0700

    New Result

--- gen.py
+++ gen.py
@@ -8,10 +8,14 @@ mode_option = {
     "20KB" : 82,
     "200KB" : 826,
     "2000KB" : 8310,
+    "1000KB" : 4150,
+    "500KB" : 2100,
 }
 # key = "20KB"
 # key = "200KB"
-key = "2000KB"
+# key = "2000KB"
+# key = "1000KB"
+key = "500KB"
 
 for _ in range(mode_option[key]):
     # my_dict = {    'foo': randint(0, 100),    'bar': {'baz': fake.name(),       'poo': float(random.randrange(155, 389))/100   } }
